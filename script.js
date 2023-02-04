493ccd7a158d7d03d54d6d73b914a6c9d12f5bd1
djsereno <djsereno91@gmail.com>
2023-02-04 13:44:42 -0800

    Update notification bell animations

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+const notificationBell = document.querySelector(".notifications");
 const favoriteButtons = document.querySelectorAll(".card-button.favorite");
 const watchButtons = document.querySelectorAll(".card-button.watch");
 
+notificationBell.addEventListener("click", toggleActive);
 favoriteButtons.forEach((item) => item.addEventListener("click", toggleActive));
 watchButtons.forEach((item) => item.addEventListener("click", toggleActive));
 
